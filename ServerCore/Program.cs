0dc8555cf0b9d3098f56e9eb7af1a55750362b62
Wise <hmh9534@naver.com>
2022-09-27 21:13:58 +0900

    Spin lock feat

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,22 +1,60 @@
 ﻿internal class Program
 {
+    private static int _num = 0;
+    private static SpinLock _lock = new SpinLock();
+
+    private static void Thread1()
+    {
+        for (int i = 0; i < 1000000; i++)
+        {
+            _lock.Acquire();
+            _num++;
+            _lock.Release();
+        }
+    }
+
+    private static void Thread2()
+    {
+        for (int i = 0; i < 1000000; i++)
+        {
+            _lock.Acquire();
+            _num--;
+            _lock.Release();
+        }
+    }
+
     private static void Main(string[] args)
     {
+        Task thread1 = new Task(Thread1);
+        Task thread2 = new Task(Thread2);
+        thread1.Start();
+        thread2.Start();
 
+        Task.WaitAll(thread1, thread2);
+
+        Console.WriteLine(_num);
     }
 }
 
 class SpinLock
 {
-    private bool _locked = false;
+    private const int expectedLockValue = 0;
+    private const int desiredLockValue = 1;
+    private volatile int _locked = 0;
 
-    private void Acquire()
+    public void Acquire()
     {
-
+        while (true)
+        {
+            if (Interlocked.CompareExchange(ref _locked, desiredLockValue, expectedLockValue).Equals(expectedLockValue))
+            {
+                break;
+            }
+        }
     }
 
-    private void Release()
+    public void Release()
     {
-
+        _locked = 0;
     }
 }
